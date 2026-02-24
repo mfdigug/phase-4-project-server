f6330f9a4ee56f61d2b488627d8dace10ac6c6b8
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-02-25 00:22:17 +1100

    configured to respond to client

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/books')
+def get_books():
+    return jsonify({
+        "books": [
+            "Book 1",
+            "Book 2",
+            "Book 3"
+        ]
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
