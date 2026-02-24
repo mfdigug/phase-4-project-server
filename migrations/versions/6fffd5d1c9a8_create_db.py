967ffefd9e24f21ebfc33498e3ea289d4f56ffd5
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-02-25 10:23:47 +1100

    initialised empty db migration

---
+++ migrations/versions/6fffd5d1c9a8_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 6fffd5d1c9a8
+Revises: 
+Create Date: 2026-02-25 10:22:58.802912
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6fffd5d1c9a8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
